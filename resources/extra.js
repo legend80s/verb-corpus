2671ce4688ad33af18a28926513b00eab0cad3c5
legend80s <chuanzongliu@gmail.com>
2021-06-28 16:12:46 +0800

    feat: add 'apply' to verb list

--- resources/extra.js
+++ resources/extra.js
@@ -1,4 +1,5 @@
 module.exports = [
+  'apply',
   'extract',
   'process',
   'recharge',
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,14 @@
 const index = require('..')
 
 describe('verb', () => {
+  it('"apply" should be included in the verb list', () => {
+    const input = 'apply';
+    const actual = index.filter(v => v === input).length;
+    const expected = 1;
+
+    expect(actual).toEqual(expected);
+  });
+
   it('"extract" should be included in the verb list', () => {
     const input = 'extract';
     const actual = index.filter(v => v === input).length;
